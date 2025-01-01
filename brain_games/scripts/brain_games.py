63a165509349943d7b9a83b940be0945e8a01b69
Виталий <ipbvv@mail.ru>
2025-01-01 20:07:27 +0300

    ADD cli.py fix brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
+import brain_games.cli
+
 def greeting():
     print('Welcome to the Brain Games!')
 
 def main():
-    greeting()
+    brain_games.cli.welcome_user()
 
 if __name__ == '__main__':
     main()
