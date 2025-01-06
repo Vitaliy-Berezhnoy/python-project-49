ae05415405c67a615c443435494b6c07ae6007f3
Виталий <ipbvv@mail.ru>
2025-01-06 23:14:58 +0300

    Fix prime.py by recommendation codeclimate

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,11 +7,7 @@ from random import randint
 def is_prime(number):
     if number <= 1: 
         return False
-    if number == 2:
-        return True
-    if number % 2 == 0:
-        return False
-    for i in range(3, int(number**0.5) + 1, 2):
+    for i in range(2, int(number**0.5) + 1):
         if number % i == 0:
             return False
     return True
